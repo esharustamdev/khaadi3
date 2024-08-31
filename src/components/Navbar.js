4c16761dfdccc457c7e41c351132804efde05a1b
esharustamdev <esharustam990@gmail.com>
2024-08-31 14:56:24 +0500

    third

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,21 +17,172 @@ const Navbar = () => {
           <ul>
             <li>
               <a href=''>Sale</a>
+              <ul className='dropdown'>
+                <li>
+                  <img
+                    src='https://pk.khaadi.com/on/demandware.static/-/Sites-storefront-catalog-pak/default/dw661b3ccb/images/megamenu/0.0-1salelive-thumbnailsfabrics1.jpg'
+                    alt=''
+                  />
+                  <a href=''>Fabric</a>
+                </li>
+                <li>
+                  <img
+                    src='https://pk.khaadi.com/on/demandware.static/-/Sites-storefront-catalog-pak/default/dwa260724c/images/megamenu/0.0-1salelive-thumbnailsRTW2.jpg'
+                    alt=''
+                  />
+                  <a href=''>Ready To Wear</a>
+                </li>
+                <li>
+                  <img
+                    src='https://pk.khaadi.com/on/demandware.static/-/Sites-storefront-catalog-pak/default/dw6ff0a62d/images/megamenu/0.0-1salelive-thumbnails1HOME.jpg'
+                    alt=''
+                  />
+                  <a href=''>Home</a>
+                </li>
+                <li>
+                  <img
+                    src='https://pk.khaadi.com/on/demandware.static/-/Sites-storefront-catalog-pak/default/dwd56f0421/images/megamenu/0.0-1salelive-thumbnailsFRAG3.jpg'
+                    alt=''
+                  />
+                  <a href=''>Fragrances</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href=''>NewIn</a>
+              <ul className='dropdown1'>
+                <li>
+                  <img
+                    src='https://pk.khaadi.com/on/demandware.static/-/Sites-storefront-catalog-pak/default/dw0c0c322f/images/megamenu/0.0-0.0-print-story-thumbnails.jpg'
+                    alt=''
+                  />
+                  <a href=''>Fabrics</a>
+                </li>
+                <li>
+                  <img
+                    src='https://pk.khaadi.com/on/demandware.static/-/Sites-storefront-catalog-pak/default/dw450d0a49/images/megamenu/0.0-London-thumbnails.jpg'
+                    alt=''
+                  />
+                  <a href=''>Ready To Wear</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href=''>Fabrics</a>
+              <ul className='dropdown2'>
+                <li className='list2'>
+                  <li>
+                    <h3>Essentials</h3>
+                    <p>2 piece</p>
+                    <p>3 piece</p>
+                  </li>
+                  <li>
+                    <h3>Signature</h3>
+                    <p>3piece</p>
+                  </li>
+                  <li>
+                    <img
+                      src='https://pk.khaadi.com/on/demandware.static/-/Library-Sites-KhaadiSharedLibrary/default/dw3261fcde/images/megamenu/0.0-eid-edit-thumbnails.jpg'
+                      alt=''
+                    />
+                  </li>
+                </li>
+              </ul>
             </li>
             <li>
               <a href=''>Ready To Wear</a>
+              <ul className='dropdown5'>
+                <li className='list5'>
+                  <li>
+                    <h3>Essential</h3>
+                    <p>Tailor 3piece</p>
+                    <p>Kurta</p>
+                    <p>Pants</p>
+                    <p>Shalwar</p>
+                    <p>Dupatta</p>
+                    <p>Chaddar</p>
+                  </li>
+                  <li>
+                    <h3>Signature</h3>
+                    <p>Tailor 3piece</p>
+                    <p>Kurta</p>
+                    <p>Pants</p>
+                    <p>Shalwar</p>
+                    <p>Dupatta</p>
+                  </li>
+                  <li>
+                    <h3>Casuals</h3>
+                    <p>Co-ord Sets</p>
+                    <p>Shirts</p>
+                    <p>T-shirts</p>
+                    <p>Blouse</p>
+                    <p>Tunic</p>
+                    <p>Maxi Dress</p>
+                    <p>Trouses</p>
+                    <p>Jeans</p>
+                    <p>Tank Top</p>
+                  </li>
+
+                  <li>
+                    <h3>Smart Casuals</h3>
+                    <p>Shirts</p>
+                    <p>T-Shirts</p>
+                    <p>Blouse</p>
+                    <p>Maxi Dress</p>
+                    <p>Trousers</p>
+                  </li>
+                  <li>
+                    <img
+                      src='https://pk.khaadi.com/on/demandware.static/-/Library-Sites-KhaadiSharedLibrary/default/dw2477c988/images/megamenu/0.0-breeze-thumbnails.jpg'
+                      alt=''
+                    />
+                  </li>
+                </li>
+              </ul>
             </li>
             <li>
               <a href=''>Fragrance</a>
+              <ul className='dropdown3'>
+                <li className='list'>
+                  <li>
+                    <a href=''>Body Mist</a>
+                  </li>
+                  <li>
+                    <a href=''>EAU DE Perfume</a>
+                  </li>
+                  <li>
+                    <img
+                      src='https://pk.khaadi.com/on/demandware.static/-/Library-Sites-KhaadiSharedLibrary/default/dwa4649993/images/megamenu/0.0-perf-thumbnails.jpg'
+                      alt=''
+                    />
+                  </li>
+                </li>
+              </ul>
             </li>
             <li>
               <a href=''>Home</a>
+              <ul className='dropdown4'>
+                <li className='list4'>
+                  <li>
+                    <h3>Coster</h3>
+                    <h3>HotPlate</h3>
+                  </li>
+                  <li>
+                    <h3>Cusion Cover</h3>
+                    <h3>Table Mat</h3>
+                  </li>
+                  <li>
+                    <h3>Envelops</h3>
+                    <h3>Table Runner</h3>
+                  </li>
+                  <li>
+                    <img
+                      src='https://pk.khaadi.com/on/demandware.static/-/Library-Sites-KhaadiSharedLibrary/default/dw4df37dac/images/megamenu/0.0-home-thumbnails-1.jpg'
+                      alt=''
+                    />
+                  </li>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
